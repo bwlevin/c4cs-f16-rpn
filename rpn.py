6e62770e53c3d4ace75fd1d52d6971218b839772
Brad Levin <bwlevin@umich.edu>
2016-10-15 20:29:54 -0400

    Added colors

--- rpn.py
+++ rpn.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import types
 import operator
-
+import readline
+import sys
+from termcolor import cprint
 
 operators = {
 	'+': operator.add,
@@ -12,6 +15,18 @@ operators = {
 	'%': operator.mod
 }
 
+def color_print(current_stack):
+	for item in current_stack:
+		if type(item) is int:
+			if item < 0:
+				cprint(item, 'red', attrs=['bold'])
+			elif item == 0:
+				cprint(item, 'yellow', attrs=['bold'])
+			else:
+				cprint(item, 'green')
+		else:
+			cprint(item, 'blue')
+
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
@@ -20,11 +35,13 @@ def calculate(myarg):
 			stack.append(token)
 		except ValueError:
 			function = operators[token]
+			cprint(token, 'blue', 'on_white')
 			arg2 = stack.pop()
 			arg1 = stack.pop()
 			result = function(arg1, arg2)
 			stack.append(result)
-		print(stack)
+		color_print(stack)
+		print("\n")
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
 	return stack.pop()
